e52d0eafe1694e47a6c257a596e17c39a5a87df6
Luis Alberto <luiss_alberto@hotmail.com>
2024-09-16 11:33:49 -0600

    re-structure go script

--- goSensor/goSensor.go
+++ goSensor/goSensor.go
@@ -10,49 +10,51 @@ import (
 
 type Temperature struct {
 	Timestamp   time.Time `json:"timestamp"`
-	SensorID    int
-	Temperature float64 `json:"temperature"`
+	SensorID    int       `json:"sensorID"`
+	Temperature float64   `json:"temperature"`
 }
 
+const (
+	mean     = 27.5
+	stdDev   = 0.1
+	filePath = "/tmp/temperature.json"
+)
+
 func main() {
-	// Set the mean and standard deviation for the Gaussian distribution
-	mean := 27.5
-	stdDev := 0.1
-	i := 0
-
+	sensorID := 0
 	for {
-		// Generate a random temperature value based on the Gaussian distribution
-		temperature := rand.NormFloat64()*stdDev + mean
-		timeStamp := time.Now()
-		i++
-		// Create a Temperature struct with timestamp and temperature values
-		data := Temperature{
-			Timestamp:   timeStamp,
-			SensorID:    i,
-			Temperature: temperature,
-		}
-
-		// Create a JSON file
-		file, err := os.Create("/tmp/temperature.json")
+		data := generateTemperatureData(&sensorID)
+		err := writeJSONToFile(data)
 		if err != nil {
-			fmt.Println("Error creating file:", err)
+			fmt.Println("Error:", err)
 			return
 		}
-
-		// Encode the data struct as JSON and write it to the file
-		encoder := json.NewEncoder(file)
-		err = encoder.Encode(data)
-		if err != nil {
-			fmt.Println("Error encoding data:", err)
-			return
-		}
-
-		file.Close()
-
-		/*fmt.Printf("ID: %d\n", i)
-		fmt.Printf("TimeSTamp: %v\n", timeStamp)
-		fmt.Printf("Temperature: %.2f\n", temperature)*/
-
-		time.Sleep(1 * time.Second) // Delay for 1 second before generating the next temperature value
+		time.Sleep(1 * time.Second)
 	}
+}
+
+func generateTemperatureData(sensorID *int) Temperature {
+	temperature := rand.NormFloat64()*stdDev + mean
+	*sensorID++
+	return Temperature{
+		Timestamp:   time.Now(),
+		SensorID:    *sensorID,
+		Temperature: temperature,
+	}
+}
+
+func writeJSONToFile(data Temperature) error {
+	file, err := os.Create(filePath)
+	if err != nil {
+		return fmt.Errorf("error creating file: %w", err)
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	err = encoder.Encode(data)
+	if err != nil {
+		return fmt.Errorf("error encoding data: %w", err)
+	}
+
+	return nil
 }
